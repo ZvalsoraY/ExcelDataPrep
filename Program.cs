e835f12cb41cc58444c85165416c4c52904d1e32
ZvalsoraY <yazamail@yandex.ru>
2021-02-24 17:13:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web;
+using NPOI.XSSF.UserModel;
+using NPOI.XSSF.Model;
+using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Model;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+
+namespace DataPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fileName = @"C:\Users\Master\Documents\C_sharp\Work\wall_test.output";
+            //Console.WriteLine(ReadFile(fileName));
+            CreateExcel();
+            Console.Read();
+        }
+
+        static string ReadFile(string fileName)
+        {
+            return File.ReadAllText(fileName);
+            //Console.WriteLine(File.ReadAllText(fileName));
+
+        }
+
+        static void CreateExcel()
+        {
+            //XSSFWorkbook wb1 = null;
+
+            using (var file = new FileStream(@"D:\test\Result.xlsx", FileMode.Create, FileAccess.ReadWrite))
+            {
+                
+                //wb1 = new XSSFWorkbook(file);
+                var wb = new XSSFWorkbook();
+                var sheet = wb.CreateSheet("RepairScope");
+                //creating cell style for header
+                //var cellStyle = CreateCellStyleForHeader(wb);
+
+
+
+                //wb1.GetSheetAt(0).GetRow(0).GetCell(0).SetCellValue("Sample");
+                //file.
+                file.Close();
+            }
+            
+
+            //using (var file2 = new FileStream(@"C:\Users\Master\Documents\C_sharp\Work\Result.xlsx", FileMode.Create, FileAccess.ReadWrite))
+            //{
+            //    wb1.Write(file2);
+            //    file2.Close();
+            //}
+
+        }
+            
+    }
+}
