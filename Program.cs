ac9208b8bde45334243bb3a25a5c14ed63d2e8c5
ZvalsoraY <yazamail@yandex.ru>
2021-02-25 22:03:27 +0300

    array prepearing

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ using NPOI.HSSF.UserModel;
 using NPOI.HSSF.Model;
 using NPOI.SS.UserModel;
 using NPOI.SS.Util;
+using NPOI.HSSF.Util;
+using System.Text.RegularExpressions;
 
 namespace DataPrep
 {
@@ -18,16 +20,45 @@ namespace DataPrep
     {
         static void Main(string[] args)
         {
-            //string fileName = @"C:\Users\Master\Documents\C_sharp\Work\wall_test.output";
+            string fileName = @"C:\Users\Master\Documents\C_sharp\Work\wall_test.output";
+            ReadFile(fileName);
             //Console.WriteLine(ReadFile(fileName));
             CreateExcel();
             Console.Read();
         }
 
-        static string ReadFile(string fileName)
+        static Array ReadFile(string fileName)
         {
-            return File.ReadAllText(fileName);
+
+            //char[] delimiterChars = { '!', ' ','\t' };
+
+            //string[] words = File.ReadAllText(fileName).Split(delimiterChars);
+            //foreach (string s in words)
+            //{
+            //    System.Console.WriteLine(s);
+            //}
+            //System.Console.WriteLine("{0} words in text:", words.Length);
+
+
+            var swords = File.ReadAllText(fileName);
+            var res = swords.Split('\t')
+                .Select(p => Regex.Split(p, " "))
+                .ToArray();
+            //foreach (string[] s in res)
+            //{
+            //    foreach (string r in s)
+            //    {
+            //        System.Console.Write(r);
+            //        //Convert.ToDouble(r);
+            //    }
+            //    System.Console.WriteLine();
+
+            //}
+
+
+            return res;
             //Console.WriteLine(File.ReadAllText(fileName));
+            //return File.ReadAllText(fileName);
 
         }
 
@@ -42,6 +73,14 @@ namespace DataPrep
                 var wb = new XSSFWorkbook();
                 var sheet = wb.CreateSheet("Test wall");
                 //creating cell style for header
+                var bStylehead = wb.CreateCellStyle();
+                bStylehead.BorderBottom = BorderStyle.Thin;
+                bStylehead.BorderLeft = BorderStyle.Thin;
+                bStylehead.BorderRight = BorderStyle.Thin;
+                bStylehead.BorderTop = BorderStyle.Thin;
+                bStylehead.Alignment = HorizontalAlignment.Center;
+                bStylehead.VerticalAlignment = VerticalAlignment.Center;
+                bStylehead.FillBackgroundColor = HSSFColor.Green.Index;
                 //var cellStyle =
                 //var cellStyle = CreateCellStyleForHeader(wb);
 
@@ -54,7 +93,10 @@ namespace DataPrep
                 row.CreateCell(4, CellType.String).SetCellValue("Hy");
                 row.CreateCell(5, CellType.String).SetCellValue("Hz");
                 row.CreateCell(6, CellType.String).SetCellValue("Hsum");
+                row.Cells[0].CellStyle = bStylehead;
 
+                //var cra = new NPOI.SS.Util.CellRangeAddress(0, 0, 0, 6);
+                //cra.
 
                 //wb1.GetSheetAt(0).GetRow(0).GetCell(0).SetCellValue("Sample");
                 //file.
