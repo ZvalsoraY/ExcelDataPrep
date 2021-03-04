1cbeb184bae45507cdf00dbd1f0ba20c1c75b29f
ZvalsoraY <yazamail@yandex.ru>
2021-03-04 10:01:39 +0300

    chart starting

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ using NPOI.SS.Util;
 using NPOI.HSSF.Util;
 using System.Text.RegularExpressions;
 using System.Globalization;
+using NPOI.SS.UserModel.Charts;
 
 namespace DataPrep
 {
@@ -118,63 +119,86 @@ namespace DataPrep
 
             using (var stream = new FileStream(@"D:\test\Result.xlsx", FileMode.Create, FileAccess.ReadWrite))
             {
-                //https://stackoverflow.com/questions/47793744/generate-excel-with-merged-header-using-npoi
-                //https://stackoverflow.com/questions/32723483/adding-a-specific-autofilter-on-a-column
+            //https://stackoverflow.com/questions/47793744/generate-excel-with-merged-header-using-npoi
+            //https://stackoverflow.com/questions/32723483/adding-a-specific-autofilter-on-a-column
+            //https://www.leniel.net/2009/10/npoi-with-excel-table-and-dynamic-chart.html
+            //https://coderoad.ru/56089507/%D0%9A%D0%B0%D0%BA-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-LineChart-%D0%BA%D0%BE%D1%82%D0%BE%D1%80%D1%8B%D0%B9-%D1%81%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D1%82-%D0%B4%D0%B2%D0%B0-CategoryAxis-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D1%83%D1%8F-apache-POI
+            //https://www.csharpcodi.com/vs2/1431/NPOI/src/NPOI.OOXML/XSSF/UserModel/Charts/XSSFLineChartData.cs/
+            //https://itnan.ru/post.php?c=1&p=525492
+            //https://overcoder.net/q/3405494/%D0%BA%D0%B0%D0%BA-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-%D0%BB%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D1%83%D1%8E-%D0%B4%D0%B8%D0%B0%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83-%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%B5-%D1%81-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%B2-%D1%82%D0%B0%D0%B1%D0%BB%D0%B8%D1%86%D0%B5-excel-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E
+            //https://gist.github.com/Bykiev/2912494f5a3e4e6f91e02c12a6d6a82d
                 //wb1 = new XSSFWorkbook(file);
                 var wb = new XSSFWorkbook();
+                //var wb = new ;
                 var sheet = wb.CreateSheet("Test wall");
-                //creating cell style for header
-                var bStylehead = wb.CreateCellStyle();
-                bStylehead.BorderBottom = BorderStyle.Thin;
-                bStylehead.BorderLeft = BorderStyle.Thin;
-                bStylehead.BorderRight = BorderStyle.Thin;
-                bStylehead.BorderTop = BorderStyle.Thin;
-                bStylehead.Alignment = HorizontalAlignment.Center;
-                bStylehead.VerticalAlignment = VerticalAlignment.Center;
-                bStylehead.FillBackgroundColor = HSSFColor.Green.Index;
-                //var cellStyle =
-                //var cellStyle = CreateCellStyleForHeader(wb);
+                ////creating cell style for header
+                //var bStylehead = wb.CreateCellStyle();
+                //bStylehead.BorderBottom = BorderStyle.Thin;
+                //bStylehead.BorderLeft = BorderStyle.Thin;
+                //bStylehead.BorderRight = BorderStyle.Thin;
+                //bStylehead.BorderTop = BorderStyle.Thin;
+                //bStylehead.Alignment = HorizontalAlignment.Center;
+                //bStylehead.VerticalAlignment = VerticalAlignment.Center;
+                //bStylehead.FillBackgroundColor = HSSFColor.Green.Index;
+                ////var cellStyle =
+                ////var cellStyle = CreateCellStyleForHeader(wb);
 
-                
-                var row = sheet.CreateRow(0);
-                row.CreateCell(0, CellType.String).SetCellValue("x");
-                row.CreateCell(1, CellType.String).SetCellValue("y");
-                row.CreateCell(2, CellType.String).SetCellValue("z");
-                row.CreateCell(3, CellType.String).SetCellValue("Hx");
-                row.CreateCell(4, CellType.String).SetCellValue("Hy");
-                row.CreateCell(5, CellType.String).SetCellValue("Hz");
-                row.CreateCell(6, CellType.String).SetCellValue("Hsum");
-                row.Cells[0].CellStyle = bStylehead;
 
-                //filling the data
-                var rowsCounter = 1;
+                //var row = sheet.CreateRow(0);
+                //row.CreateCell(0, CellType.String).SetCellValue("x");
+                //row.CreateCell(1, CellType.String).SetCellValue("y");
+                //row.CreateCell(2, CellType.String).SetCellValue("z");
+                //row.CreateCell(3, CellType.String).SetCellValue("Hx");
+                //row.CreateCell(4, CellType.String).SetCellValue("Hy");
+                //row.CreateCell(5, CellType.String).SetCellValue("Hz");
+                //row.CreateCell(6, CellType.String).SetCellValue("Hsum");
+                //row.Cells[0].CellStyle = bStylehead;
 
-                string fileName = @"C:\Users\Master\Documents\C_sharp\Work\wall_test.output";
-                var fileData = ReadFile(fileName);
+                ////filling the data
+                //var rowsCounter = 1;
 
-                foreach (var rowData in inputArray)
-                {
-                    var rowD = sheet.CreateRow(rowsCounter++);
-                    var dCounter = 0;
-                    foreach(var d in rowData)
-                    {
-                        rowD.CreateCell(dCounter++, CellType.Numeric).SetCellValue(Double.Parse(d.ToString().Replace(@".", @",")));
-                    }
-                }
-                
-                ////var row = sheet.CreateRow(0);
-                //foreach (Array rowData in fileData)
+                //string fileName = @"C:\Users\Master\Documents\C_sharp\Work\wall_test.output";
+                //var fileData = ReadFile(fileName);
+
+                //foreach (var rowData in inputArray)
                 //{
                 //    var rowD = sheet.CreateRow(rowsCounter++);
-                //    //rowD.CreateCell(0, CellType.String).SetCellValue(rowData.Length);
-                //    for (int i = 1; i < rowData.Length; i++)
+                //    var dCounter = 0;
+                //    foreach (var d in rowData)
                 //    {
-                //        rowD.CreateCell(i-1, CellType.Numeric).SetCellValue(Double.Parse(rowData.GetValue(i).ToString().Replace(@".", @",")));
+                //        rowD.CreateCell(dCounter++, CellType.Numeric).SetCellValue(Double.Parse(d.ToString().Replace(@".", @",")));
                 //    }
-                    
                 //}
-                //sheet.SetAutoFilter(CellRangeAddress.ValueOf("A:C"));
-                
+
+
+                //object[][] plotData = new object[2][];
+
+                //plotData[0] = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+                //plotData[1] = new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+                //IWorkbook wb = new XSSFWorkbook();
+                //ISheet sheet = new SheetBuilder(wb, plotData).Build();
+                IDrawing Drawing = sheet.CreateDrawingPatriarch();
+
+                IClientAnchor anchor = Drawing.CreateAnchor(0, 0, 0, 0, 8, 1, 18, 16);
+                IChart chart = Drawing.CreateChart(anchor);
+
+                IChartAxis bottomAxis = chart.ChartAxisFactory.CreateCategoryAxis(AxisPosition.Bottom);
+                IChartAxis leftAxis = chart.ChartAxisFactory.CreateValueAxis(AxisPosition.Left);
+
+                var chartData =
+                        chart.ChartDataFactory.CreateLineChartData<double, double>();
+
+                IChartDataSource<double> xs = DataSources.FromNumericCellRange(sheet, CellRangeAddress.ValueOf("A1:G1"));
+                IChartDataSource<double> ys = DataSources.FromNumericCellRange(sheet, CellRangeAddress.ValueOf("A2:G2"));
+                var series = chartData.AddSeries(xs, ys);
+                series.SetTitle("test");
+                chart.Plot(chartData, bottomAxis, leftAxis);
+
+
+
+
+
                 wb.Write(stream);
                 //file.Close();
             }
